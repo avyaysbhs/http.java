ad863e855f612aacb8397be791a93f0ed6dd1912
avyaysbhs <44980422+avyaysbhs@users.noreply.github.com>
2019-01-02 14:22:08 -0500

    Update Http.java

--- src/Http.java
+++ src/Http.java
@@ -1,6 +1,13 @@
 public class Http {
-  private abstract class Server() {
-    
+  private abstract class Server {
+    public HTTPServer listen(int port) {
+      (new HTTPServer(port) { 
+        @Override
+        public void handle(HttpRequest req, HttpResponse res) {
+
+        }
+      }).start();
+    }
   }
   
   public Http.Server createServer(HttpRequestHandler handler) {
