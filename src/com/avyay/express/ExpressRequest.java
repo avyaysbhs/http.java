b4949ba47665bda6c2dfcc4dcee599293e88a64c
Avyay Natarajan <avyay@Avyays-MBP.lan>
2019-01-06 22:27:34 -0500

    works now because of jesus's blessings

--- src/com/avyay/express/ExpressRequest.java
+++ src/com/avyay/express/ExpressRequest.java
@@ -3,31 +3,40 @@ package com.avyay.express;
 import com.avyay.http.java.HttpRequest;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class ExpressRequest {
     private HttpRequest request;
-    private HashMap<String, String> params;
-    private HashMap<String, String> qArgs;
+    private HashMap<String, String> _params;
+    private HashMap<String, String> _query;
 
     public ExpressRequest(HttpRequest request, HashMap<String, String> params, String source) {
-        this.params = params;
-        qArgs = new HashMap<>();
+        _params = params;
+        _query = new HashMap<>();
         if (source.contains("?")) {
             source = source.substring(source.indexOf("?") + 1, source.length());
             String[] args = source.split("&");
             for (String arg: args) {
                 String key = arg.substring(0, arg.indexOf("="));
                 String value = arg.substring(arg.indexOf("=") + 1, arg.length());
-                qArgs.put(key, value);
+                _query.put(key, value);
             }
         }
     }
 
     public String param(String key) {
-        return params.get(key);
+        return _params.get(key);
+    }
+
+    public Set<String> params() {
+        return _params.keySet();
     }
 
     public String query(String key) {
-        return qArgs.get(key);
+        return _query.get(key);
+    }
+
+    public Set<String> query() {
+        return _query.keySet();
     }
 }
--- src/com/avyay/express/Test.java
+++ src/com/avyay/express/Test.java
@@ -1,15 +1,15 @@
 package com.avyay.express;
 
 public class Test {
-    public Test() {
-
-    }
-
     public static void main(String[] args) {
         Express.App app = new Express.App();
 
         app.get("/index", (req, res) -> {
             res.sendFile("index.html", "text/html");
+        });
+
+        app.get("/image", (req, res) -> {
+            res.sendFile("test.gif", "image/gif");
         });
 
         app.get("/", (req, res) -> {
@@ -18,14 +18,15 @@ public class Test {
 
         Express.Router rt = new Express.Router();
 
-        rt.get("/image/:name", (req, res) -> {
-            System.out.println(req.param("name"));
-            res.sendFile("test.gif", "image/gif");
+        rt.get("/hello/:name/:lname", (req, res) -> {
+            for (String q: req.query()) {
+                Express.debug.log(q, req.query(q));
+            }
+            res.send("Hello " + req.param("name") + " " + req.param("lname") + "!");
         });
 
         app.use(rt);
 
         app.listen(3000);
-        new Test();
     }
 }
